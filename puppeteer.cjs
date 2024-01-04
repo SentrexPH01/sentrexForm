736a9260a07bc962dee762f97635a029fc090dfc
SentrexPH01 <paulh@sentrex.com>
2024-01-04 16:43:13 -0500

    added puppeteer cfg file

--- puppeteer.cjs
+++ puppeteer.cjs
@@ -5,6 +5,7 @@ import { join } from 'path';
  */
 const puppeteerConfig = {
   // Changes the cache location for Puppeteer.
+  // eslint-disable-next-line no-undef
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
 
